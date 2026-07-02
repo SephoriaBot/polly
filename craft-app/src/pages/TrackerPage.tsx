7c0b53fa39c13f3bca845c8d98efcd870f361cda
SephoriaBot <withallofthisimdone@gmail.com>
2026-07-02 16:22:42 -0400

    Update TrackerPage.tsx

--- craft-app/src/pages/TrackerPage.tsx
+++ craft-app/src/pages/TrackerPage.tsx
@@ -4,7 +4,7 @@ import PeriodLogForm from '../pages/PeriodLogForm';
 import WeightLogForm from '../pages/WeightLogForm';
 import TrackerChart from '../pages/TrackerChart';
 import { TRACKER_CONFIG } from '../data/trackerConfig';
-import { TrackerType, PeriodValue } from '../types';
+import { TrackerType, PeriodValue } from '../types/tracker';
 import { getTrackerLogsInRange } from '../../api/trackerApi';
 
 function todayISO() {
