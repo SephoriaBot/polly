8f793d76fe9d0f3ceffdcf60fc275dc4adaf19cf
SephoriaBot <withallofthisimdone@gmail.com>
2026-07-29 00:57:54 -0400

    fix import

--- craft-app/src/components/tracker/TrackerChart.tsx
+++ craft-app/src/components/tracker/TrackerChart.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import Icon from '../Icon';
-import Lantern from "../components/Lantern";
+import Lantern from "../Lantern";
 import {
   ResponsiveContainer,
   BarChart,
