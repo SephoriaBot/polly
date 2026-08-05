7e17f3715f4b3a9f433123530190c283302d0a6b
SephoriaBot <withallofthisimdone@gmail.com>
2026-08-05 15:46:09 -0400

    Update EmptyState.tsx

--- craft-app/src/components/EmptyState.tsx
+++ craft-app/src/components/EmptyState.tsx
@@ -16,7 +16,7 @@ export default function EmptyState({ image, message, subMessage }: EmptyStatePro
       textAlign: 'center',
       padding: '2rem 1rem',
     }}>
-      <img src={image} alt="" style={{ width: '180px', marginBottom: '1rem' }} />
+      <img   src={image}   alt=""   style={{     width: '80px',     height: 'auto',     marginBottom: '1rem',     display: 'block',   }} />
       <p style={{
         fontFamily: 'var(--font-heading)',
         fontSize: '1.1rem',
