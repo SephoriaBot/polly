f193169447b2c79956dc12416d62bd87b29f9d19
SephoriaBot <withallofthisimdone@gmail.com>
2026-08-06 16:42:09 -0400

    Update Icon.tsx

--- craft-app/src/components/Icon.tsx
+++ craft-app/src/components/Icon.tsx
@@ -57,6 +57,7 @@ export default function Icon({ name, size = 20, alt = '', className, style }: Ic
         verticalAlign: '-0.2em',
         objectFit: 'contain',
         maxWidth: 'none',
+        alignItems: 'center',
         ...style,
       }}
     />
