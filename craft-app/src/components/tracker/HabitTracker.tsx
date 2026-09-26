84cc6ff07b8b68fb57357be6d69b15dabca5e38d
SephoriaBot <withallofthisimdone@gmail.com>
2026-09-26 17:18:17 -0400

    Update HabitTracker.tsx

--- craft-app/src/components/tracker/HabitTracker.tsx
+++ craft-app/src/components/tracker/HabitTracker.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useMemo, useState, useCallback } from 'react';
 import { supabase } from '../../lib/supabase';
+import './HabitTracker.css';
+
 
 // ---------- types ----------
 
